5b5fc97d5e4b4cbfba92f8ea771c0d9b043f5dc6
sirvisuresh <sureshsirvi578@gmail.com>
2018-01-20 15:14:48 +0530

    Add files via upload

---
+++ varavarb.py
@@ -0,0 +1,13 @@
+varA = 32
+varB = 'djfdjs'
+if isinstance(varA,str) or isinstance(varB,str):
+     print("string involved")
+elif varA>varB :
+     print("bigger")
+elif varA==varB :
+     print("equal")
+else:
+     print("smaller")
+
+
+ 
